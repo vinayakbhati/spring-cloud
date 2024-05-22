ba6d196a2738fc385be0241b07a9ce5480a0d3ce
vinayakbhati <77834579+vinayakbhati@users.noreply.github.com>
2024-05-23 00:46:04 +0530

    spring-cloud-config-server

---
+++ spring-cloud-config-server/src/test/java/com/micro/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
